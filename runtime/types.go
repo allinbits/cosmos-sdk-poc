501914be54961f6f810f9007abb1e419e17d23a7
Jonathan Gimeno <jgimeno@gmail.com>
2021-04-27 23:15:56 +0200

    remove meta

--- runtime/types.go
+++ runtime/types.go
@@ -1,7 +1,6 @@
 package runtime
 
 import (
-	"github.com/fdymylja/tmos/module/meta/v1alpha1"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -13,7 +12,6 @@ type Type interface {
 // StateTransition is a type which is used to cause state transitions
 type StateTransition interface {
 	Type
-	GetTransitionMeta() *v1alpha1.TransitionMeta
 }
 
 // Name returns the unique name for the Type
