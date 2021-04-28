e6f6ed5abdf4520d4c17b4239e57831c4d35fc9f
Jonathan Gimeno <jgimeno@gmail.com>
2021-04-28 09:51:29 +0200

    get id

--- runtime/types.go
+++ runtime/types.go
@@ -17,7 +17,7 @@ type StateTransition interface {
 // StateObject defines an object which is saved in the state
 type StateObject interface {
 	Type
-	ID
+	GetId() ID
 }
 
 // ID defines the unique identification of an StateObject.
