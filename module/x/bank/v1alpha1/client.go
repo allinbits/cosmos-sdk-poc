44752b12080941f8fb9b395dec6dbaba950f798d
Frojdi Dymylja <frojdi.dymylja@gmail.com>
2021-04-30 18:10:26 +0200

    add: fee deduction

--- module/x/bank/v1alpha1/client.go
+++ module/x/bank/v1alpha1/client.go
@@ -1,9 +1,14 @@
 package v1alpha1
 
 import (
+	coin "github.com/fdymylja/tmos/module/core/coin/v1alpha1"
 	"github.com/fdymylja/tmos/runtime/meta"
 	"github.com/fdymylja/tmos/runtime/module"
 )
+
+func NewClient(c module.Client) *Client {
+	return &Client{c: c}
+}
 
 type Client struct {
 	c module.Client
@@ -13,4 +18,12 @@ func (c *Client) GetBalance(address string) (*Balance, error) {
 	balance := new(Balance)
 	err := c.c.Get(meta.NewStringID(address), balance)
 	return balance, err
+}
+
+func (c *Client) Send(sender, recipient string, amount []*coin.Coin) error {
+	return c.c.Deliver(&MsgSendCoins{
+		FromAddress: sender,
+		ToAddress:   recipient,
+		Amount:      amount,
+	})
 }
